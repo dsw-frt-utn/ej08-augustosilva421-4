48006e59e3c45df16a15e6235fc3c3f23ccd9a3e
augustosilva421 <luciano.silva421@gmail.com>
2026-05-04 03:21:15 -0300

    Agregar resolucion problema 1

---
+++ Dsw2026Ej8/ProductHelperB.cs
@@ -0,0 +1,10 @@
+namespace Dsw2026Ej8
+{
+    internal partial class ProductHelper
+    {
+        public string ObtenerPrecioFormateado(decimal price)
+        {
+            return $"${price}";
+        }
+    }
+}
